8bdedb9784e5b61d8cbb760817c17189cffece50
unknown <Tarens18@yahoo.com>
2019-04-23 11:13:37 -0400

    comment out new .js

--- APItest.js
+++ APItest.js
@@ -1,22 +1,33 @@
-var url= 'http://api.open-notify.org/iss-now.json';
-var issX = 0;
-var issY = 0;
+var url = 'http://api.open-notify.org/iss-now.json';
+var img = 'Mercator_projection_SW.jpg';
 
-function setup(){
-    createCanvas (600,400);
-    loadJSON(url, gotData);
+
+
+function setup() {
+    createCanvas(1600, 1400);
+   /* ctx.drawImage(img)*/
+    loadJSON(url, getData, 'jsonp');
 }
 
-function gotData(data){
-    var lat = data.iss_position.latitude;
-    var long = data.iss_position.longitude;
+/*function setup() {
+    
+}*/
+
+function getData(data) {
+    var lat = 'data.iss_position.latitude';
+    var long = 'data.iss_position.longitude';
     issX = map(lat, -90, 90, 0, width);
     issY = map(long, -90, 90, 0, height);
 
 }
 
-function draw(){
-    ellipse(issx, issy, 4, 4);
+function draw() {
+    background(img);
+    ellipse(issX, issY, 24, 24);
+
+
 }
 
-setInterval(function(){ gotData(); }, 3000);
+setInterval(function () { getData(); }, 3000);
+
+
